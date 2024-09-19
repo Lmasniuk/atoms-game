d5b509d893bbd73a83521fc0ab1965de28746e21
Logan Masniuk <logan.masniuk@gmail.com>
2024-09-19 11:56:17 -0700

    beginning to set up player turns

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import Board from "./components/Board/Board";
 
-import './App.css'
-import Board from './components/Board/Board'
+import { GameState } from "./types/GameState";
 
 function App() {
+    const [gameState, setGameState] = useState<GameState>({ playersTurn: 1 });
 
-  return (
-    <>
-      <h1>Atoms The Game</h1>
-      <Board />
-    </>
-  )
+    return (
+        <>
+            <h1>Atoms The Game</h1>
+            <h2>Player {gameState.playersTurn}'s Turn</h2>
+            <Board gameState={gameState} setGameState={setGameState} />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,53 +1,58 @@
-import './Board.scss'
+import "./Board.scss";
 
-import {NUMBER_OF_ROWS , NUMBER_OF_COLUMNS } from '../../gameConfig/gameConfig';
+import { NUMBER_OF_ROWS, NUMBER_OF_COLUMNS } from "../../gameConfig/gameConfig";
 
-import { useState } from 'react';
+import { useState } from "react";
 import { Cell } from "../../types/Cell";
 
-import {createInitialCellState } from '../../utils/cellUtils';
-import BoardRow from '../BoardRow/BoardRow';
+import { createInitialCellState } from "../../utils/cellUtils";
+import BoardRow from "../BoardRow/BoardRow";
 
-export default function Board() {
-    
-    const createInitialBoardState  = (): Cell[][] =>{
+import { GameState } from "../../types/GameState";
+
+interface BoardProps {
+    gameState: GameState;
+    setGameState: React.Dispatch<React.SetStateAction<GameState>>;
+}
+
+export default function Board({ gameState, setGameState }: BoardProps) {
+    const createInitialBoardState = (): Cell[][] => {
         const boardState: Cell[][] = [];
 
         // for(let columnIndex =0; columnIndex < NUMBER_OF_COLUMNS; columnIndex++){
-        for(let rowIndex =0; rowIndex < NUMBER_OF_ROWS; rowIndex++){
+        for (let rowIndex = 0; rowIndex < NUMBER_OF_ROWS; rowIndex++) {
             const row: Cell[] = [];
 
-            
             // for(let rowIndex =0; rowIndex < NUMBER_OF_ROWS; rowIndex++){
-            for(let columnIndex =0; columnIndex < NUMBER_OF_COLUMNS; columnIndex++){
-                const cell = createInitialCellState(rowIndex,columnIndex);
+            for (
+                let columnIndex = 0;
+                columnIndex < NUMBER_OF_COLUMNS;
+                columnIndex++
+            ) {
+                const cell = createInitialCellState(rowIndex, columnIndex);
                 row.push(cell);
             }
             boardState.push(row);
         }
         return boardState;
-    }
+    };
 
-    const initialBoardState: Cell[][] = createInitialBoardState()
-    
-    const [boardState,setBoardState] = useState(initialBoardState)
+    const initialBoardState: Cell[][] = createInitialBoardState();
+
+    const [boardState, setBoardState] = useState(initialBoardState);
 
     return (
         <div className="board">
-            {/* {
-            boardState.map(
-                (boardRow, rowIndex)=>(
-                    <BoardRow  boardRow={boardRow}  boardState={boardState} setBoardState={setBoardState} key={rowIndex}/>
-                )
-            )} */}
-            {
-            boardState.map((boardRow, rowIndex) =>{
-                return(
-                    <BoardRow boardRow={boardRow}  boardState={boardState} setBoardState={setBoardState} key={rowIndex} />
-                )
-            }
-                
-            )}
+            {boardState.map((boardRow, rowIndex) => {
+                return (
+                    <BoardRow
+                        boardRow={boardRow}
+                        boardState={boardState}
+                        setBoardState={setBoardState}
+                        key={rowIndex}
+                    />
+                );
+            })}
         </div>
     );
 }
